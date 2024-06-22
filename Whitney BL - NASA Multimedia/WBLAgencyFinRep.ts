2189724440d323ea36ad4758181a348c6c69df97
whitney-bridwell-lewis <wbl.work.email@gmail.com>
2024-06-22 17:33:39 -0500

    Add WhitneyPage

---
+++ Whitney BL - NASA Multimedia/WBLAgencyFinRep.ts
@@ -0,0 +1,26 @@
+import { BasePage } from "../basePage";
+import {By} from 'selenium-webdriver'; 
+const fs = require('fs');
+
+export class WBLAgencyFinRep extends BasePage {
+    // locators go here
+    footer: By = By.xpath('(//span[text()="Humans in Space"])');
+    agencyFinRep: By = By.xpath('(//span[text() = "Agency Financial Reports"])');
+
+    // constructor goes next so we can use our class as an object in our test
+    constructor(){
+        super({url: 'https://www.nasa.gov/'});
+    };
+    
+    // Methods next for repeatable steps that we use often.
+
+    async search(searchThing: string) {
+        return this.setInput(this.footer, `${searchThing}`);
+    };
+    async getResults() {
+        return this.getText(this.footer);
+    };
+    async clickAgencyFinRep(){
+        return this.driver.findElement(this.agencyFinRep);
+    };
+};
